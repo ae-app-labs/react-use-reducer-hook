0ae404c6d87040619a0474dc858e05a2d9fdc2cb
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-05-01 22:13:48 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// An initial state
+const initialState = {
+  name : 'John Doe',
+  age: 42
 }
 
-export default App;
+// Reducer to mutate the state
+function reducer(state, action) {
+  switch(action.type) {
+    case "NAME":
+      return { ...state, name: action.payload }
+    case "AGE" :
+      return { ...state, age: action.payload }
+    default:
+      return state
+  }
+}
+
+export default function App() {
+  /*
+  With useReducer, inside the reducer function, the changes of state are handled, and now it focuses on user behavior woth dispatch. The "state" of usereducer is an object, with each property being an individual state. 
+  */
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return(<div className='App'>
+      <br /> <br/>
+      Name: {state.name} <br/>
+      <input value={state.name} onChange={ (e) => {
+        dispatch( { type: "NAME", payload: e.target.value })
+      }}
+      />
+
+      <br />
+      Name: {state.age} <br />
+      <input value={state.age} onChange={ (e) => {
+        dispatch( {type: "AGE", payload:e.target.value })
+      }}
+      />
+  </div>)
+
+}
